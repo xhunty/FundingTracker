c9f95df30996bee78122652cbeb3739b80d6d18e
Константин Губин <xhunty@gmail.com>
2024-11-20 21:37:58 +0300

    Add project files.

---
+++ FundingTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using FundingTracker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHostBuilder builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .UseSystemd();
+IHost host = builder.Build();
+host.Run();
---
+++ FundingTracker/Worker.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundingTracker;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> logger;
+    public Worker(ILogger<Worker> logger)
+    {
+        this.logger = logger;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
